629bdfcd283f6c8381e58dc30e010f1ded2b7a37
OhIlKang <zxc3613@naver.com>
2019-12-06 12:10:32 +0900

    마이 피쉬

---
+++ Assets/Common/UnityAdsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAdsManager : MonoBehaviour
+{
+    string gameId = "3382661";
+    
+    void Start()
+    {
+        
+    }
+        
+    void Update()
+    {
+        
+    }
+}
